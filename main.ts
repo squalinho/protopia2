8cf4613ad71d46bb50898a48a53640cd220b4a78
squale <squalinho@gmail.com>
2021-07-14 22:09:02 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-let heroX = scene.screenWidth() / 2
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    heroX = heroX + 1
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    heroX = heroX - 1
+})
+let heroX = 0
+heroX = scene.screenWidth() / 2
 let heroY = scene.screenHeight() / 2
 let Hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
-Hero.setPosition(heroX, heroY)
+game.onUpdate(function () {
+    Hero.setPosition(heroX, heroY)
+})
