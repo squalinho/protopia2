15318d7232dd451777d0ae524cb5dd3c94c04140
squale <squalinho@gmail.com>
2021-07-14 19:13:29 +0200

    Mise à jour pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,14 +3,32 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "image1":
+            case "myImage":return img`
+. . . . f f f f . . . . 
+. . f f e e e e f f . . 
+. f f e e e e e e f f . 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f b 1 1 1 1 b f 4 e 
+4 d f 1 1 1 1 1 1 f d 4 
+4 4 f 6 6 6 6 6 6 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "forestMonkeyWalkRight":
+            case "heroSpr":
             case "anim1":return [img`
 . . . . . . . f f f f f . . . . 
 . . . . . . f e e e e e f . . . 
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+let heroX = scene.screenWidth() / 2
+let heroY = scene.screenHeight() / 2
+let Hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
+Hero.setPosition(heroX, heroY)
